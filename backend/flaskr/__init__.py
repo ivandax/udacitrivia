897993c3100874f0ee2d381fd4cbd776e803c014
ivan.dario <ivan.dario@nexiona.com>
2022-05-02 22:56:58 +0200

    Add initial files with compiling version of flask app

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -97,6 +97,15 @@ def create_app(test_config=None):
     Create error handlers for all expected errors
     including 404 and 422.
     """
+    
+    @app.route("/")
+    def hello():
+        return jsonify({"message": "Hello world"})
 
     return app
 
+app = create_app()
+
+if __name__ == '__main__':
+    app.init_db()
+    app.run(debug=True)
